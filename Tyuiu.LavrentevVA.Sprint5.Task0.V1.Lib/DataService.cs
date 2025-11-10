9ebc6452f67be376e8dba33c5d78b7906b2d5a44
dimka <dimka556.dim4@gmail.com>
2025-11-10 23:11:07 +0500

    Task0

--- Tyuiu.LavrentevVA.Sprint5.Task0.V1.Lib/DataService.cs
+++ Tyuiu.LavrentevVA.Sprint5.Task0.V1.Lib/DataService.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.LavremtevVA.Sprint5.Task0.V1.Lib
         public string SaveToFileTextData(int x)
         {
             // Дано выражение  F(x) = x^3 + 2x^2 + 5x + 4  вычислить его значение при x = 3, результат сохранить в текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до трёх знаков после запятой. Название проектов (консольного приложения, библиотеки классов, тестового модуля) оформить по шаблону в соответствии с вариантом. Интерфейс консольного приложения оfформить по шаблону. Для составления пути используйте Path.Combine() (не слеши) Директорию берите через Path.GetTempPath() Или, чтобы не составлять путь можно использовать Path.GetTempFileName(), он сразу создает файл с случайным именем в темп папке
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
             double z = Math.Pow(x, 3) + 2 * Math.Pow(x, 2) + 5 * x + 4;
             z = Math.Round(z, 3);
             File.WriteAllText(path, Convert.ToString(z));
