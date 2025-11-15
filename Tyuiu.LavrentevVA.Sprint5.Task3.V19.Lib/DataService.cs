f5161c6b90f2e00b0e3950ec9cd047c78ca53198
dimka <dimka556.dim4@gmail.com>
2025-11-15 16:58:50 +0500

    Task3

---
+++ Tyuiu.LavrentevVA.Sprint5.Task3.V19.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.LavrentevVA.Sprint5.Task3.V19.Lib
+{
+    public class DataService : ISprint5Task3V19
+    {
+        public string SaveToFileTextData(int x)
+        {
+            // Дано выражение вычислить его значение при x = 3, результат сохранить в бинарный файл OutPutFileTask3.bin и вывести на консоль. Округлить до трёх знаков после запятой. y(x) = 2x^2 - 1 / sqrt(x^2 -2) Название проектов (консольного приложения, библиотеки классов, тестового модуля) оформить по шаблону в соответствии с вариантом. Интерфейс консольного приложения оформить по шаблону. 
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+            FileInfo FileInfo = new FileInfo(path);
+            bool fileExists = FileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+            double y = 2 * Math.Pow(x, 2) - 1 / Math.Sqrt(Math.Pow(x, 2) - 2);
+            y = Math.Round(y, 3);
+            using (BinaryWriter writer = new BinaryWriter(File
+                    .Open(path, FileMode.Create)))
+            {
+                writer.Write(y);
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.LavrentevVA.Sprint5.Task3.V19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.LavrentevVA.Sprint5.Task3.V19.Lib;
+
+namespace Tyuiu.LavrentevVA.Sprint5.Task3.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 3;
+            string path = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + path);
+            Console.WriteLine("Создан!");
+
+            Console.ReadKey();
+        }
+    }
+}
