60356d87e706bdfa7d54b460835c6cd27b36428e
dimka <dimka556.dim4@gmail.com>
2025-11-17 21:00:15 +0500

    Task4

---
+++ Tyuiu.LavrentevVA.Sprint5.Task4.V29.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.LavrentevVA.Sprint5.Task4.V29.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.LavrentevVA.Sprint5.Task4.V29/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.LavrentevVA.Sprint5.Task4.V29.Lib;
+
+namespace Tyuiu.LavrentevVA.Sprint5.Task4.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\DataSprint5\InPutDataFileTask4V29.txt";
+            double result = ds.LoadFromDataFile(path);
+            Console.WriteLine("Результат: " + result);
+            Console.ReadKey();
+        }
+    }
+}
+
