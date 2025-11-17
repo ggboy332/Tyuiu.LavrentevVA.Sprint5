a69a8217948f70d53e7d47facbca8f5bdadf7d30
dimka <dimka556.dim4@gmail.com>
2025-11-17 21:45:43 +0500

    tASK5

---
+++ Tyuiu.LavrentevVA.Sprint5.Task5.V23.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.LavrentevVA.Sprint5.Task5.V23.Lib
+{
+    public class DataService : ISprint5Task5V23
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string data = File.ReadAllText(path);
+            string[] numbers = data.Split(' ');
+            double min = double.MaxValue;
+            foreach (string number in numbers)
+            {
+                if (double.TryParse(number, NumberStyles.Any, CultureInfo.InvariantCulture, out double value))
+                {
+                    if (value < min)
+                    {
+                        min = value;
+                    }
+                }
+            }
+
+            min = Math.Round(min, 3);
+            return min;
+        }
+    }
+}
---
+++ Tyuiu.LavrentevVA.Sprint5.Task5.V23/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.LavrentevVA.Sprint5.Task5.V23.Lib;
+
+namespace Tyuiu.LavrentevVA.Sprint5.Task4.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask5V23.txt";
+            double result = ds.LoadFromDataFile(path);
+            Console.WriteLine("Результат: " + result);
+            Console.ReadKey();
+        }
+    }
+}
+
