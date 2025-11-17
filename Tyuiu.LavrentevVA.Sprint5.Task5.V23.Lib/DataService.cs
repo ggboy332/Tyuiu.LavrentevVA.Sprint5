8a942f9e1ce211a1326b63283fa57ab35a20a6b9
dimka <dimka556.dim4@gmail.com>
2025-11-17 21:51:48 +0500

    Tas5

--- Tyuiu.LavrentevVA.Sprint5.Task5.V23.Lib/DataService.cs
+++ Tyuiu.LavrentevVA.Sprint5.Task5.V23.Lib/DataService.cs
@@ -1,29 +1,28 @@
 ﻿using System.IO;
-using System.Globalization;
 using tyuiu.cources.programming.interfaces.Sprint5;
-
 namespace Tyuiu.LavrentevVA.Sprint5.Task5.V23.Lib
 {
     public class DataService : ISprint5Task5V23
     {
         public double LoadFromDataFile(string path)
         {
-            string data = File.ReadAllText(path);
-            string[] numbers = data.Split(' ');
-            double min = double.MaxValue;
-            foreach (string number in numbers)
+            double res = double.MaxValue;
+            using (StreamReader reader = new StreamReader(path))
             {
-                if (double.TryParse(number, NumberStyles.Any, CultureInfo.InvariantCulture, out double value))
+                string line;
+                while ((line = reader.ReadLine()) != null)
                 {
-                    if (value < min)
+                    if (double.TryParse(line, out double number))
                     {
-                        min = value;
+                        if (number < res)
+                        {
+                            res = number;
+                        }
                     }
                 }
             }
-
-            min = Math.Round(min, 3);
-            return min;
+            res = Math.Round(res, 3);
+            return res;
         }
     }
 }
