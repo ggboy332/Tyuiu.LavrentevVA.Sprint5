21f9c57103aa7a0b8e2cd5b726cae32cce268f46
dimka <dimka556.dim4@gmail.com>
2025-11-18 09:51:51 +0500

    t7

--- Tyuiu.LavrentevVA.Sprint5.Task7.V29.Lib/DataService.cs
+++ Tyuiu.LavrentevVA.Sprint5.Task7.V29.Lib/DataService.cs
@@ -11,11 +11,11 @@ namespace Tyuiu.LavrentevVA.Sprint5.Task7.V29.Lib
             // Читаем содержимое файла
             string content = File.ReadAllText(path);
 
-            // Удаляем однозначные sdчисла с помощью регулярного выражения
+            // Удаляем однозначные числа с помощью регулярного выражения
             string result = Regex.Replace(content, @"\b\d\b", "");
 
-            // Формируем путь для выходного файла в той же папке, что и входной файл
-            string outputPath = Path.Combine(Path.GetDirectoryName(path), "OutPutDataFileTask7V29.txt");
+            // Сохраняем файл в текущей рабочей директории
+            string outputPath = "OutPutDataFileTask7V29.txt";
 
             // Записываем результат в выходной файл
             File.WriteAllText(outputPath, result);
--- Tyuiu.LavrentevVA.Sprint5.Task7.V29/Program.cs
+++ Tyuiu.LavrentevVA.Sprint5.Task7.V29/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.ConstrainedExecution;
-using System.Text;
+﻿using System;
 using Tyuiu.LavrentevVA.Sprint5.Task7.V29.Lib;
 
 namespace Tyuiu.LavrentevVA.Sprint5.Task7.V29
@@ -13,9 +10,20 @@ namespace Tyuiu.LavrentevVA.Sprint5.Task7.V29
             DataService ds = new DataService();
 
             string path = @"C:\DataSprint5\InPutDataFileTask7V29.txt";
+
+            // Для локального тестирования создаем директорию и файл если их нет
+            string testContent = "сегодня 12 завтра 4 послезавтра 34 и потом 4 5 6";
+            Directory.CreateDirectory(@"C:\DataSprint5");
+            File.WriteAllText(path, testContent);
+
             string resultPath = ds.LoadDataAndSave(path);
 
             Console.WriteLine("Результат сохранен в файл: " + resultPath);
+
+            // Выводим содержимое результата для проверки
+            string resultContent = File.ReadAllText(resultPath);
+            Console.WriteLine("Содержимое результата: " + resultContent);
+
             Console.ReadKey();
         }
     }
