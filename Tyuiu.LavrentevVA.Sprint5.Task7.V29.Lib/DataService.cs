44ee4d9542d0e4b428b58320d9a1399ecd590278
dimka <dimka556.dim4@gmail.com>
2025-11-18 09:40:57 +0500

    Tas7

--- Tyuiu.LavrentevVA.Sprint5.Task7.V29.Lib/DataService.cs
+++ Tyuiu.LavrentevVA.Sprint5.Task7.V29.Lib/DataService.cs
@@ -11,7 +11,6 @@ namespace Tyuiu.LavrentevVA.Sprint5.Task7.V29.Lib
             //Дан файл С:\DataSprint5\InPutDataFileTask7V29.txt (файл взять из архива согласно вашему варианту. Создать папку в ручную С:\DataSprint5\ и скопировать в неё файл) в котором есть набор символьных данных.Удалить все однозначные числа из файла.Полученный результат сохранить в файл OutPutDataFileTask7V29.txt. 
             string pathsaveFile = $@"C:\DataSprint5\OutPutDataFileTask7V29.txt";
             string content = File.ReadAllText(path);
-            // Используем регулярное выражение для удаления однозначных чисел
             string result = Regex.Replace(content, @"\b\d\b", "");
             File.WriteAllText(pathsaveFile, result);
             return pathsaveFile;
